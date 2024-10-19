31942ec91951d4b027ed7d2d02fdf453d8b23207
David Santiago Rojas Páez <santiagodrp37@gmail.com>
2024-10-18 22:52:07 -0500

    Hud de vidas
    Hud de vidas de player, con su script para visualizar el numero de vidas en la pantalla

---
+++ Assets/UIScripts/UIController.cs
@@ -0,0 +1,46 @@
+
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static UIController instance;
+    public Image[] hearts;
+    public Sprite heartFull, heartEmpty;
+
+    public int health;
+
+    private void Awake() {
+
+        instance = this;    
+    }
+
+    
+    void Start()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].sprite = heartFull;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //actualiza el contador de vida
+    [ContextMenu("UpdateHealth")] 
+    public void UpdateHealthDisplay()
+    {
+
+        if (health >=0)
+        {
+            hearts[health].sprite = heartEmpty;
+            health--;
+        }
+    }
+}
